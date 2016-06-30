23375f14def6d33522ebe5a61f43a5efd26042f4
zhnhou <zhn.hou@gmail.com>
2016-06-29 20:26:42 -0500

    start first nn practice

---
+++ neural-network/handwritten-digits/mnist.py
@@ -0,0 +1,10 @@
+import numpy as np
+import os
+import struct
+
+def load_mnist_train(path):
+    image_web_path = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz'
+    label_web_path = 'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz'
+
+
+
