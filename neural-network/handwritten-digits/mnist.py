60b972cda30e2f74ee7035352c53d019de38b742
zhnhou <zhn.hou@gmail.com>
2016-06-29 21:10:34 -0500

    get mnist train ready

--- neural-network/handwritten-digits/mnist.py
+++ neural-network/handwritten-digits/mnist.py
@@ -2,9 +2,31 @@ import numpy as np
 import os
 import struct
 
+def fetch_mnist_train(path):
+    
+    image_file = 'train-images-idx3-ubyte.gz'
+    label_file = 'train-labels-idx1-ubyte.gz'
+
+    web_path = 'http://yann.lecun.com/exdb/mnist/'
+
+    if ( not os.path.isfile(path+image_file) ):
+        os.system("wget -O "+path+"/"+image_file+" "+web_path+image_file)
+        os.system("gzip "+path+"/"+image_file+" -d") # decompress and delete .gz file
+    if ( not os.path.isfile(path+label_file) ):
+        os.system("wget -O "+path+"/"+label_file+" "+web_path+label_file)
+        os.system("gzip "+path+"/"+label_file+" -d")
+
+## load the mnist training data
 def load_mnist_train(path):
-    image_web_path = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz'
-    label_web_path = 'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz'
+    image_file = path+"/train-images-idx3-ubyte"
+    label_file = path+"/train-labels-idx1-ubyte"
 
+    with open(label_file) as lb_unit:
+        magic, n = struct.unpack('>II', lb_unit.read(8))
+        labels = np.fromfile(lb_unit, dtype=np.uint8)
 
+    with open(image_file) as im_unit:
+        magic, num, rows, cols = struct.unpack('>IIII', im_unit.read(16))
+        images = np.fromfile(im_unit, dtype=np.uint8).reshape(len(labels), 784)
 
+    return images, labels
