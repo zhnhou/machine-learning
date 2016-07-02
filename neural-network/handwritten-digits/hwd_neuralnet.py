552b76cd8a853c4d86e9f3140029e45d72ecedcb
zhnhou <zhn.hou@gmail.com>
2016-07-02 17:46:56 -0500

    forward cost ready

--- neural-network/handwritten-digits/hwd_neuralnet.py
+++ neural-network/handwritten-digits/hwd_neuralnet.py
@@ -3,10 +3,12 @@ from scipy.special import expit
 
 class NeuralNet(object):
 
-    def __init__(self, num_output, num_feature, num_hidden=30):
+    def __init__(self, num_output, num_feature, num_hidden=30, lambda1=0.0, lambda2=0.0):
         self.num_output = num_output
         self.num_feature = num_feature
         self.num_hidden = num_hidden
+        self.lambda1 = lambda1
+        self.lambda2 = lambda2
 
     def _encode_labels(self, y, n_output):
         ones = np.zeros((n_output, y.shape[0]))
@@ -32,6 +34,14 @@ class NeuralNet(object):
 
         return X_new
 
+    def _L1_reg(self, lambda_, theta1, theta2):
+        ## excluing the bias unit when doing regularization
+        return lambda_/2.0 * (np.abs(theta1[:,1:]).sum() + np.abs(theta2[:,1:]).sum())
+
+    def _L2_reg(self, lambda_, theta1, theta2):
+        ## excluing the bias unit when doing regularization
+        return lambda_/2.0 * (np.sum(theta1[:,1:]**2) + np.sum(theta2[:,1:]**2))
+
     def _feedforward(self, X, theta1, theta2):
         a1 = self._add_bias_unit(X)
         z2 = a1.dot(theta1.T)
@@ -44,5 +54,10 @@ class NeuralNet(object):
         return a1, z2, a2, z3, a3
 
     def _costfunction(self, y_enc, output, theta1, theta2):
-        cost = - y_enc * np.log(output) - (1-y_enc) * np.log(1 - output)
+        cost = np.sum(- y_enc * np.log(output) - (1-y_enc) * np.log(1 - output))
+
+        ## regularization ##
+        cost += self._L1_reg(self.lambda1, theta1, theta2) + self._L2_reg(self.lambda2, theta1, theta2)
+
+        return cost / y_enc.shape[1]
 
